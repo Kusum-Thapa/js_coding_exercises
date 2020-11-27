a67d6c1739fc4a78954e812b4a8737527ebf4828
Kusum <kusumthapa_magar@hotmail.com>
2020-11-27 20:04:04 +0000

    Implemented exercise 005 - Count1sAnd0s

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -3,16 +3,25 @@ const findNextNumber = (nums, n) => {
   if (n === undefined) throw new Error("n is required");
   
   var numPos = nums.indexOf(n);
-  
+
   if(numPos == -1 || numPos == nums.length-1)  return null;
   else  return nums[numPos + 1];
-}
+};
 
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
-};
 
+  var zeroCount = 0;
+  var oneCount = 0;
+
+  for(i = 0; i < str.length; i++)
+  {
+    if(str[i] == 0) zeroCount++;
+    else if(str[i] == 1) oneCount++;    
+  }
+  const result = {1 : oneCount, 0 : zeroCount};
+  return result;
+};
 const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
   // Your code here!
